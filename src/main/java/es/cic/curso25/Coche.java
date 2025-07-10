713d80c3557f0588aafeff756c0438bb972d2fce
HeDichoQueNo <no@nomail.no>
2025-07-10 11:20:25 +0200

    Se añade clase coche y test en disabled

---
+++ src/main/java/es/cic/curso25/Coche.java
@@ -0,0 +1,30 @@
+package es.cic.curso25;
+
+public class Coche {
+
+    public int getVelocidad() {
+
+        // TODO:Pendiente de completar getVelocidad
+
+        throw new UnsupportedOperationException("No implementado todavía");
+    }
+
+    public double getConsumo() {
+        // TODO:Pendiente de completar getConsumo
+
+        throw new UnsupportedOperationException("No implementado todavía");
+    }
+
+    public int acelerar(int incrementoVelocidad){
+        //TODO:Pendiente de completar acelearar
+
+        throw new UnsupportedOperationException("No implementado todavía");
+    }
+
+    public int frenar(int decrementoVelocidad){
+        //TODO:Pendiente de completar decremento Velocidad
+
+        throw new UnsupportedOperationException("No implementado todavía");
+    }
+
+}
---
+++ src/test/java/es/cic/curso25/CocheTest.java
@@ -0,0 +1,51 @@
+package es.cic.curso25;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class CocheTest {
+
+    private Coche cut;
+
+    @BeforeEach
+    public void setUp() {
+        cut = new Coche();
+    }
+
+    @Disabled
+    @Test
+    void testAcelerar() {
+        cut.acelerar(34);
+        int velocidadFinal = cut.acelerar(23);
+        assertEquals(57, velocidadFinal);
+    }
+
+    @Disabled
+    @Test
+    void testFrenar() {
+        cut.acelerar(50);
+        int velocidadFinal = cut.frenar(20);
+        assertEquals(30, velocidadFinal);
+    }
+
+    @Disabled
+    @Test
+    void testGetConsumo() {
+        cut.acelerar(50);
+        double consumoActual = cut.getConsumo();
+        assertEquals(2.15, consumoActual);
+
+    }
+
+    @Disabled
+    @Test
+    void testGetVelocidad() {
+        cut.acelerar(50);
+        int velocidadFinal = cut.getVelocidad();
+        assertEquals(50, velocidadFinal);
+
+    }
+}
